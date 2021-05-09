702cbee0cee96b0d39160173bec52381c6219c05
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-05-09 18:50:57 +0545

    explicit animation

--- project 6/ContentView.swift
+++ project 6/ContentView.swift
@@ -8,23 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var enabled = false
+    
+    @State private var dragAmount = CGSize.zero //we need some state to store the amount of their drag:
+    
+
     var body: some View {
         
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 300)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+           
             
-    Button("Tap Me") {
-        self.enabled.toggle()
-    }
-    .frame(width: 200, height: 200)
-    .background(enabled ? Color.red : Color.blue)
-    .animation(nil)
-    .foregroundColor(.white)
-    .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-    .animation(.interpolatingSpring(stiffness: 10, damping: 1))
-    
-   
-   
-            
+            .gesture(
+            DragGesture()
+                .onChanged{ self.dragAmount = $0.translation} // lets us run a closure whenever the user moves their finger.
+                .onEnded{ _ in
+                    withAnimation(.spring()) { // this is explicit animation 
+                        self.dragAmount = .zero } //lets us run a closure when the user lifts their finger off the screen, ending the drag.
+                    }
+                    
+            )
+           // its an implicit animation
+//            .animation(.spring())
     }
 }
 
